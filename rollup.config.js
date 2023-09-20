dfa06f6aa857a392624d477f541dff8bdaaf22d4
vvhybe <vvhybe@hotmail.com>
2023-09-20 22:46:03 +0100

    coding & bundelling envirment

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import typescript from "rollup-plugin-typescript2";
+import json from "@rollup/plugin-json";
+
+export default {
+  input: "index.ts",
+  output: [
+    {
+      file: "dist/hu.min.cjs",
+      format: "cjs", // using CJS format for Node.js.
+      sourcemap: true,
+    },
+    {
+      file: "dist/hu.min.js",
+      format: "esm", // using ESM format for Browsers and sweet able for script tag
+      sourcemap: true,
+    }
+  ],
+
+  plugins: [
+    resolve(),
+    commonjs(),
+    json(),
+    babel({
+      exclude: "node_modules/**",
+    }),
+    typescript({
+      tsconfig: "tsconfig.json",
+    })
+  ],
+};
